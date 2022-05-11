0f0e80113e4cedfacc9cfcba764d2e58b6c07634
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-05-11 17:00:07 -0300

    feat(dto): add @PastOrPresent annotation to dataFabricacao

--- src/main/java/io/github/pedrobicudo/desafio/rest/dto/VeiculoCadastroDTO.java
+++ src/main/java/io/github/pedrobicudo/desafio/rest/dto/VeiculoCadastroDTO.java
@@ -6,10 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -28,6 +25,7 @@ public class VeiculoCadastroDTO {
     private String modelo;
 
     @NotNull(message = "A data de fabricação do veículo deve ser informada")
+    @PastOrPresent(message = "A data de fabricação deve estar no passado ou presente")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataFabricacao;
 
