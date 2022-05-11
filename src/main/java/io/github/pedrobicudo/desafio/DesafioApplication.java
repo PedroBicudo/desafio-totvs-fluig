96a119aa51765f6e4407d1c64dd4e2ddc937ec70
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-05-11 10:46:08 -0300

    initial commit

---
+++ src/main/java/io/github/pedrobicudo/desafio/DesafioApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedrobicudo.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
