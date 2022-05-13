061cde1785c2583806435b7dcd75685fed8ddaa5
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-05-13 10:30:22 -0300

    docs(service): add comment describing rounding action

--- src/main/java/io/github/pedrobicudo/desafio/model/domain/services/implementations/PrevisaoService.java
+++ src/main/java/io/github/pedrobicudo/desafio/model/domain/services/implementations/PrevisaoService.java
@@ -32,9 +32,11 @@ public class PrevisaoService implements IPrevisaoService {
                     double litrosCidade = totalKmCidade / v.getConsumoMedioCidadeKML();
                     double litrosRodovia = totalKmRodovia / v.getConsumoMedioRodoviaKML();
 
+                    // Arredonda para duas casas decimais
                     BigDecimal litrosTotalGasolina = new BigDecimal(litrosCidade + litrosRodovia)
                             .setScale(2, RoundingMode.HALF_EVEN);
 
+                    // Arredonda para duas casas decimais
                     BigDecimal precoTotalGasolina = precoGasolina
                             .multiply(litrosTotalGasolina)
                             .setScale(2, RoundingMode.HALF_EVEN);
